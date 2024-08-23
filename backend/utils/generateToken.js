5473b3de8e5428c41877c5bb2887ea8ef039b15d
sarathsathish07 <sarathsathish1998@gmail.com>
2024-08-23 23:35:31 +0530

    cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,15 +1,17 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const generateToken = (res,userId)=>{
-  const token = jwt.sign({userId}, process.env.JWT_SECRET,{
-    expiresIn:"30d",
-  })
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 
   res.cookie('jwt', token, {
-    httpOnly: true,  
-    sameSite: 'strict',  
-    maxAge: 30 * 24 * 60 * 60 * 1000,  
-  })
-}
+   
+    sameSite: 'lax', 
 
-export default generateToken
+
+    maxAge: 30 * 24 * 60 * 60 * 1000, 
+  });
+};
+
+export default generateToken;
