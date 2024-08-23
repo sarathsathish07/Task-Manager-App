aee83c4ebb63384271e428df0bc5ba1bc383a7bd
sarathsathish07 <sarathsathish1998@gmail.com>
2024-08-23 20:59:06 +0530

    build files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // The root is the current folder (frontend)
   plugins: [react()],
+  build: {
+    outDir: '../dist', // Output directory will be in the project root (../dist)
+    emptyOutDir: true, // Clear the output directory before building
+  },
   server: {
     port: 3000,
     proxy: {
-       '/api':{
-        target: "http://localhost:5000",
-        changeOrigin: true
-       }
+      '/api': {
+        target: 'http://localhost:5000', // Your backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
